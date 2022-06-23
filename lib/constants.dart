3040621f4a5d90f2a0fb629e161e5528d13cde9f
gabrieltiburcio <gabrieltiburcio@laudolab.com.br>
2022-06-23 17:25:58 -0300

    .

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 class Contants {
-  static const String kApiRoute = 'http://192.168.0.11:3002';
+  static const String kApiRoute = 'https://gvt-controle-vendas.herokuapp.com';
   static const String kAppId = '4963926130024193';
   static const String kMeliRoute =
       'https://auth.mercadolivre.com.br/authorization?response_type=code&client_id=$kAppId';
